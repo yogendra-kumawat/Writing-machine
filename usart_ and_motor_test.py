592f22bc6ba05a0a84506022e747423da01fe4d4
yogendra-kumawat <yogendrakumawat2024@gmail.com>
2026-05-24 18:06:40 +0530

    Update usart_ and_motor_test.py

--- usart_ and_motor_test.py
+++ usart_ and_motor_test.py
@@ -5,19 +5,10 @@ BAUD_RATE = 9600
 try:
     ser = serial.Serial(PORT, BAUD_RATE, timeout=1)
     print(f"--- Connected to {PORT} at {BAUD_RATE} baud ---")
-    print("Type characters to send to STM32 (Ctrl+C to exit)\n")
-
-    # Start a background thread to listen for incoming data
-    # thread = threading.Thread(target=read_from_port, args=(ser,), daemon=True)
-    # thread.start()
 
     while True:
-        # Get user input
-        for i in range (0,180):
-            j=i
-        
-            k=i
-            msg = "A"+str(i).zfill(3)+str(j).zfill(3)+str(k).zfill(3)
+         for i in range (0,180):
+            msg = "A"+str(i).zfill(3)+str(i).zfill(3)+str(i).zfill(3)#to test the usart and mtors 
         # Send data (encoded to bytes)
             ser.write(msg.encode('utf-8'))
             time.sleep(0.02)
